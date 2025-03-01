b7e36d5e49eaef5497eb9976c7de22b962699340
Manoj Kumar Mutukuloju <manojcareer18@gmail.com>
2025-02-28 21:54:43 -0500

    command invoker class created

---
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInvoker : MonoBehaviour
+{
+    private Stack<ICommand> commandRegistry;
+
+    public void ExecuteCommand(ICommand command)=> command.Execute();
+
+
+    public void RegisterCommand(ICommand command)=> commandRegistry.Push(command);
+
+}
