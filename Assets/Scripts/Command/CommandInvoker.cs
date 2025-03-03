59224d5c98da28c0a860a3d404cc18439258cf18
Manoj Kumar Mutukuloju <manojcareer18@gmail.com>
2025-03-03 05:36:59 -0500

    Implemented Undo function in invoker class

--- Assets/Scripts/Command/CommandInvoker.cs
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Command.Main;
 public class CommandInvoker : MonoBehaviour
 {
     private Stack<ICommand> commandRegistry = new Stack<ICommand>();
 
     public void ExecuteCommand(ICommand command)=> command.Execute();
+
+    public void Undo()
+    {
+        if (!IsRegistryEmpty() && CommandBelongsToActivePlayer())
+        {
+            commandRegistry.Pop().Undo();
+        }
+        
+    }
+
+    private bool CommandBelongsToActivePlayer()
+    {
+       return (commandRegistry.Peek() as IUnitCommand).commandData.ActorPlayerID == GameService.Instance.PlayerService.ActivePlayerID;
+    }
+    private bool IsRegistryEmpty()
+    {
+        return commandRegistry.Count==0;
+    }
 
 
     public void RegisterCommand(ICommand command)=> commandRegistry.Push(command);
